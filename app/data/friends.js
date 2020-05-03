3cf25487e890f82fd601f374874b16dafa3819d8
Patrick O'Brien <patricknathanobrien@gmail.com>
2020-05-03 16:45:26 -0400

    Images

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,12 +1,12 @@
 var friends = [
     {
         "name":"James",
-        "photo":"Photo by Pixabay from Pexels",
+        "photo":"https://image.shutterstock.com/image-photo/handsome-unshaven-young-darkskinned-male-600w-640011838.jpg",
         "scores":[ 5, 1, 4, 4, 5, 1, 2, 5, 4, 1 ]
     },
     {
         "name":"Andrea",
-        "photo":"Photo by Andrea Piacquadio from Pexels",
+        "photo":"https://image.shutterstock.com/image-photo/people-beauty-lifestyle-concept-shot-600w-662171107.jpg",
         "scores":[3, 2, 5, 4, 5, 1, 2, 5, 4, 1]
     }    
 ];
