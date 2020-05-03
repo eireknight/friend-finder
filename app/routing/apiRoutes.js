a0cf8cb1d08d547797084c4835914725e520c1e0
Patrick O'Brien <patricknathanobrien@gmail.com>
2020-05-03 16:20:58 -0400

    Change to server file

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,7 +1,7 @@
 var friends = require("../data/friends");
 
 module.exports = function(app){
-    app.get("/api/friends",function(req,res){
+    app.get("../api/friends",function(req,res){
         res.json(friends);
     });
     app.post("/api/friends",function(req,res){
