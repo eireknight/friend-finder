62d1b1f1ee5e725aa0a9513c0a3aba1a535ec0eb
Patrick O'Brien <patricknathanobrien@gmail.com>
2020-05-03 17:08:24 -0400

    Added more friends, Final Commit

--- app/data/friends.js
+++ app/data/friends.js
@@ -9,6 +9,21 @@ var friends = [
         "photo":"https://image.shutterstock.com/image-photo/people-beauty-lifestyle-concept-shot-600w-662171107.jpg",
         "scores":[3, 2, 5, 4, 5, 1, 2, 5, 4, 1]
     }    
+    {
+        "name":"Daniel",
+        "photo":"https://image.shutterstock.com/image-photo/adult-hispanic-man-wearing-glasses-600w-1166318779.jpg",
+        "scores":[ 2, 1, 3, 2, 1, 5, 4, 3, 2, 3 ]
+    },
+    {
+        "name":"Mrs. Happypants",
+        "photo":"https://image.shutterstock.com/image-photo/grandmother-portrait-set-studio-concepts-600w-797669914.jpg",
+        "scores":[ 5, 5, 5, 5, 5, 5, 5, 5, 5, 5 ]
+    },
+    {
+        "name":"Mr. Grumper",
+        "photo":"https://image.shutterstock.com/image-photo/portrait-angry-senior-frowning-looking-260nw-417855667.jpg",
+        "scores":[ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 ]
+    },
 ];
 
 module.exports = friends;
